bf82ae0dcac69b1b17eff2396a29df6a92172093
Shilpa Mali <malishilpa037@gmail.com>
2024-11-19 16:43:01 +0530

    Code is added

---
+++ GitDemo/src/com/velocity/Example1.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Example1 {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("ram");
+        al.add("Shyam");
+        al.add("avi");
+        System.out.println(al);
+    }
+}
